f861556d71966b1dd8312ef793224325c523c574
Rich Gautier <rgautier@gmail.com>
2017-08-19 15:43:28 +0000

    Updates to remove sensitive info from Code file

--- Part4/findgiveaways.py
+++ Part4/findgiveaways.py
@@ -6,30 +6,27 @@ import os
 import sys
 import smtplib
 from email.mime.text import MIMEText
-
-me = 'SemiAmusingBot@vps1.richgautier.com'
-you = 'rgautier@gmail.com'
+from giveaways_config import *
 
 # Create the Reddit instance
-user_agent = "Windows:ReadReactBot:0.1 (by /u/rgautier)"
 r = praw.Reddit(user_agent=user_agent)
 
 # Have we run this code before? If not, create an empty list
-if not os.path.isfile("posts_replied_to.txt"):
+if not os.path.isfile(file_processed_record):
     posts_replied_to = []
 
 # If we have run the code before, load the list of posts we have replied to
 else:
     # Read the file into a list and remove any empty values
-    with open("posts_replied_to.txt", "r") as f:
+    with open(file_processed_record, "r") as f:
         posts_replied_to = f.read()
         posts_replied_to = posts_replied_to.split("\n")
         posts_replied_to = filter(None, posts_replied_to)
 
 # Get the top 5 values from our subreddit
-subreddit = r.get_subreddit('pcmasterrace')
+subreddit = r.get_subreddit(subreddit_name)
 
-f = open("posts_replied_to.txt","a")
+f = open(file_processed_record,"a")
 for submission in subreddit.get_hot(limit=50):
     print "reviewing: " + submission.title.encode('utf-8')
 
@@ -37,7 +34,7 @@ for submission in subreddit.get_hot(limit=50):
     if submission.id not in posts_replied_to:
 
         # Do a case insensitive search
-        if re.search("giveaway", submission.title, re.IGNORECASE):
+        if re.search(search_text, submission.title, re.IGNORECASE):
             # Reply to the post
             try:
                 print submission.title.encode('utf-8')
