4a83af1d033341d97cb940483802827d7c72bd80
Rich Gautier <rgautier@gmail.com>
2017-08-19 11:52:37 -0400

    Create giveaways_config.py

---
+++ Part4/giveaways_config.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+#mail address for email to come from
+me = '<from address here>'
+
+#mail address for bot to mail alerts to
+you = '<to address here>'
+
+#Reddit API friendly user agent
+user_agent = "<Write this correctly according to Reddit Bot Rules>"
+
+#File name to store a list of posts we've processed already
+file_processed_record = "posts_replied_to.txt"
+
+#The subreddit to look at
+subreddit_name = '<put the subreddit to watch here, e.g. pcmasterrace>'
+
+#Text to search for
+search_text = "<your search term>"
